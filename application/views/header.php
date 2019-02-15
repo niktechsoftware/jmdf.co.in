8c37fcad7dd99c8b6b10b98024bff85511a27f3a
shraddha1507 <sgupta.chk1507@gmail.com>
2019-02-15 17:44:51 +0530

    add

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,8 @@
          <div class="col-md-4">
              <img src="<?php echo base_url();?>assets/Res_Images/jmd2.jpg" style="margin-left:40px">
                                   </div>
-           <div class="col-md-3"><img src="<?php echo base_url();?>assets/Res_Images/jmd.jpg" style="margin-left:-90px">
+           <div class="col-md-3"><img src="<?php echo base_url();?>assets/Res_Images/jmd.jpg" style="margin-left:-85px">
+               
            </div>
                          <!-- <div class="col-md-8">
                         <br><br><br>
